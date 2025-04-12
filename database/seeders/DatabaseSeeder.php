70bf3837ea96b328b61fb7c434d2f60b65471d91
A.aka <hirayama.akatsuki@gmail.com>
2025-04-12 21:51:47 +0900

    投稿用のテーブルにダミーデータを追加2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(5)->create();
 
+         \App\Models\Post::factory(5)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
